fcbeb01bc30c0bb5807b1fd443d7b9056b567bef
mhernangilp <mhernangilp@gmail.com>
2023-06-12 11:52:08 +0200

    optimized sorting 5 elements

--- src/sort_small_functions.c
+++ src/sort_small_functions.c
@@ -31,13 +31,47 @@ static void	sort_3(t_stack **a)
 	}
 }
 
+static int	get_stack_pos(t_stack **a, int value)
+{
+	t_stack	*aux;
+	t_stack	*ant;
+	int	pos;
+
+	pos = 0;
+	aux = *a;
+	if (value < get_smallest_num(*a, 0) || value > get_biggest_num(*a, 0))
+		while (*aux -> value != get_smallest_num(*a, 0) && aux != NULL)
+		{
+			aux = aux -> next;
+			pos++;
+		}
+	else
+	{
+		ant = *a;
+		while (ant -> next != NULL)
+			ant = ant -> next;
+		while (!(*ant -> value < value && value < *aux -> value))
+		{
+			ant = aux;
+			aux = aux -> next;
+			pos++;
+		}
+	}
+	return (pos);
+}
+
 static void	place_stack(t_stack **a, int value)
 {
 	t_stack	*end;
 
 	if (value < get_smallest_num(*a, 0) || value > get_biggest_num(*a, 0))
 		while (*(*a) -> value != get_smallest_num(*a, 0))
-			rotate(a, 'a');
+		{
+			if (get_stack_pos(a, value) < stack_len(*a) / 2) 
+				rotate(a, 'a');
+			else
+				reverse(a, 'a');
+		}
 	else
 	{
 		end = *a;
@@ -45,12 +79,30 @@ static void	place_stack(t_stack **a, int value)
 			end = end -> next;
 		while (!(*end -> value < value && value < *(*a) -> value))
 		{
-			rotate(a, 'a');
+			if (get_stack_pos(a, value) < stack_len(*a) / 2)
+				rotate(a, 'a');
+			else
+				reverse(a, 'a');
 			end = *a;
 			while (end -> next != NULL)
 				end = end -> next;
 		}
 	}
+}
+
+static int	get_sort_pos(t_stack **a)
+{
+	t_stack	*aux;
+	int	pos;
+
+	aux = *a;
+	pos = 0;
+	while (*aux -> value != get_smallest_num(*a, 0))
+	{
+		aux = aux -> next;
+		pos++;
+	}
+	return (pos);
 }
 
 static void	sort_5(t_stack **a, t_stack **b)
@@ -65,7 +117,12 @@ static void	sort_5(t_stack **a, t_stack **b)
 		push(a, b, 'a');
 	}
 	while (!is_sorted(*a))
-		rotate(a, 'a');
+	{
+		if (get_sort_pos(a) < stack_len(*a) / 2)
+			rotate(a, 'a');
+		else
+			reverse(a, 'a');
+	}
 }
 
 void	sort_small(t_stack **a, t_stack **b)
