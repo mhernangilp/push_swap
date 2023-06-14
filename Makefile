a7931ce91fbfed2e04d5326b8dab4826ce833529
Marcos Hernangil Prats <mhernang@c1r15s6.42madrid.com>
2023-06-14 14:45:21 +0200

    fixed some norminette errors

--- Makefile
+++ Makefile
@@ -6,15 +6,22 @@
 #    By: mhernang <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/31 19:16:20 by mhernang          #+#    #+#              #
-#    Updated: 2023/05/31 19:58:02 by mhernang         ###   ########.fr        #
+#    Updated: 2023/06/14 14:39:06 by mhernang         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 
 SRC = src/main.c \
-	  src/exit_program.c \
-	  src/check_args.c
+	  src/basic_functions.c \
+	  src/basic_functions_2.c \
+	  src/check_args.c \
+	  src/check_args_utils.c \
+	  src/initialize_stacks.c \
+	  src/initialize_stacks_utils.c \
+	  src/movement_functions.c \
+	  src/sort_big_functions.c \
+	  src/sort_small_functions.c
 
 OBJ = ${SRC:.c=.o}
 
--- push_swap.h
+++ push_swap.h
@@ -6,34 +6,32 @@
 /*   By: mhernang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 19:12:04 by mhernang          #+#    #+#             */
-/*   Updated: 2023/05/31 20:33:04 by mhernang         ###   ########.fr       */
+/*   Updated: 2023/06/14 14:44:46 by mhernang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#             include <stdio.h>
-
 # include <stdlib.h>
 # include <unistd.h>
 
 typedef struct s_stack
 {
-	int		*value;
-	int		*index;
-	int		*arr;
-	int		*arr_size;
+	int				*value;
+	int				*index;
+	int				*arr;
+	int				*arr_size;
 	struct s_stack	*next;
 }	t_stack;
 
 //Check args
-int	check_args(char **argv, int argc);
+int		check_args(char **argv, int argc);
 
 //Check args utils
-int	is_sign(char c);
-int	nbstr_cmp(const char *s1, const char *s2);
-int	is_digit(char c);
+int		is_sign(char c);
+int		nbstr_cmp(const char *s1, const char *s2);
+int		is_digit(char c);
 
 //Initialize stacks
 void	initialize_stacks(t_stack **a, t_stack **b, char **argv, int argc);
@@ -56,16 +54,16 @@ void	reverse(t_stack **stack, char name);
 
 //Basic functions
 t_stack	*new_node(char *value);
-int	stack_len(t_stack *stack);
-int	is_sorted(t_stack *stack);
-int	get_biggest_num(t_stack *stack, int mode);
-int	get_smallest_num(t_stack *stack, int mode);
+int		stack_len(t_stack *stack);
+int		is_sorted(t_stack *stack);
+int		get_biggest_num(t_stack *stack, int mode);
+int		get_smallest_num(t_stack *stack, int mode);
 
 //Basic functions 2
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	exit_program(t_stack **a, t_stack **b);
-int	get_sort_pos(t_stack **a);
-int	get_stack_pos(t_stack **a, int value);
+int		get_sort_pos(t_stack **a);
+int		get_stack_pos(t_stack **a, int value);
 
 //Extra functions
 void	print_stack(char name, t_stack *stack);
